2a93e437c83b514af4e7cb8e4443a2f23058274a
Your Name <you@example.com>
2022-04-09 14:46:19 -0300

    criando o primeiro elemento com react

---
+++ src/components/Headers.js
@@ -0,0 +1,7 @@
+import React from 'react'; //importação do react
+//(Header)começa com letra maiuscula pois é um componente segundo uma "convenção"
+//linha 4 função de importação de html
+const Header = () => (<h1>Megabank</h1>);
+//função para exporta
+export default Header;
+
